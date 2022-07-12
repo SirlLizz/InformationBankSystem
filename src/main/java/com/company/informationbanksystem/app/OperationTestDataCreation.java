03c6b1b9dc128eda20fdccecd64d84c8162714dc
Ilya <sirllizz@gmail.com>
2022-07-13 01:48:34 +0400

    Correct code

--- src/main/java/com/company/informationbanksystem/app/OperationTestDataCreation.java
+++ src/main/java/com/company/informationbanksystem/app/OperationTestDataCreation.java
@@ -1,186 +1,115 @@
 package com.company.informationbanksystem.app;
 
 import com.company.informationbanksystem.entity.Account;
+import com.company.informationbanksystem.entity.Operation;
+import com.google.common.collect.Lists;
 import io.jmix.core.DataManager;
-import io.jmix.core.SaveContext;
+import io.jmix.core.Messages;
 import io.jmix.core.TimeSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.*;
 
 @Component("OperationTestDataCreation")
 public class OperationTestDataCreation {
-//
-//    private static final Logger log = LoggerFactory.getLogger(OperationTestDataCreation.class);
-//
-//    protected final TimeSource timeSource;
-//    protected final DataManager dataManager;
-//
-//    public OperationTestDataCreation(TimeSource timeSource, DataManager dataManager) {
-//        this.timeSource = timeSource;
-//        this.dataManager = dataManager;
-//    }
-//
-//    public void createData() {
-//
-//        if (visitsExists()) {
-//            log.info("Account found in DB. Account Test data generation is skipped...");
-//            return;
-//        }
-//        log.info("No Accounts found in the DB. Accounts Test data will be created...");
-//        int operationCreatedCount = commit(createOperations());
-//        log.info(String.format("%d Accounts created", operationCreatedCount));
-//
-//    }
-//
-//    List<Account> createOperations() {
-//
-//        final Boolean archive;
-//        final String name;
-//        final currency
-//        final List<User> allNurses = employeeRepository.
-//        final List<Pet> allPets = list(Pet.class);
-//
-//        return Stream.concat(
-//                        createPastVisits(allPets, allNurses),
-//                        createFutureVisits(allPets, allNurses)
-//                )
-//                .collect(Collectors.toList());
-//    }
-//
-//    private Stream<Visit> createPastVisits(List<Pet> possiblePets, List<User> possibleNurses) {
-//        return IntStream.range(0, petclinicTestdataProperties.getVisitStartAmountPastDays())
-//                .mapToObj(value -> timeSource.now().minusDays(value).toLocalDate())
-//                .map(localDate -> createVisitsForDate(localDate, possiblePets, possibleNurses))
-//                .flatMap(Collection::stream)
-//                .filter(Objects::nonNull);
-//    }
-//
-//    private Stream<Visit> createFutureVisits(List<Pet> possiblePets, List<User> possibleNurses) {
-//        return IntStream.range(1, petclinicTestdataProperties.getVisitStartAmountFutureDays() + 1)
-//                .mapToObj(i -> createVisitsForDate(
-//                        timeSource.now().plusDays(i).toLocalDate(),
-//                        amountForFutureDate(i),
-//                        possiblePets,
-//                        possibleNurses
-//                ))
-//                .flatMap(Collection::stream)
-//                .filter(Objects::nonNull);
-//    }
-//
-//    private int amountForFutureDate(int i) {
-//        int max = petclinicTestdataProperties.getVisitStartAmountFutureDays() + 1;
-//        return (int) ((double) (max - i) / max * petclinicTestdataProperties.getAmountPerDay());
-//    }
-//
-//    private List<Visit> createVisitsForDate(LocalDate localDate, List<Pet> possiblePets, List<User> possibleNurses) {
-//        return IntStream.range(0, petclinicTestdataProperties.getAmountPerDay())
-//                .mapToObj(i -> createVisit(localDate, possiblePets, possibleNurses))
-//                .collect(Collectors.toList());
-//    }
-//
-//    private List<Visit> createVisitsForDate(LocalDate localDate, int amount, List<Pet> possiblePets, List<User> possibleNurses) {
-//        return IntStream.range(0, amount)
-//                .mapToObj(i -> createVisit(localDate, possiblePets, possibleNurses))
-//                .collect(Collectors.toList());
-//    }
-//
-//    private boolean visitsExists() {
-//        return !list(Visit.class).isEmpty();
-//    }
-//
-//    private int commit(List<Account> operations) {
-//        SaveContext saveContext = new SaveContext();
-//        operations.forEach(dataManager::save);
-//        dataManager.save(saveContext);
-//
-//        return operations.size();
-//    }
-//
-//    Visit createVisit(
-//            LocalDate date,
-//            List<Pet> possiblePets,
-//            List<User> possibleNurses
-//    ) {
-//
-//        VisitEventRange visitEventRange = randomVisitDateTime.randomVisitEventRange(date);
-//
-//        if (visitEventRange.isEmpty()) {
-//            return null;
-//        }
-//
-//        Visit visit = dataManager.create(Visit.class);
-//
-//        visit.setTreatmentStatus(treatmentStatusFor(date));
-//
-//        if (nurseShouldBeAssigned(date)) {
-//            visit.setAssignedNurse(randomOfList(possibleNurses));
-//        }
-//
-//        visit.setPet(randomOfList(possiblePets));
-//        visit.setType(randomVisitType());
-//        visit.setDescription(randomDescription());
-//
-//        visit.setVisitStart(visitEventRange.getVisitStart());
-//        visit.setVisitEnd(visitEventRange.getVisitEnd());
-//
-//        return visit;
-//    }
-//
-//    private boolean nurseShouldBeAssigned(LocalDate date) {
-//        return date.isBefore(timeSource.now().toLocalDate().plusWeeks(1).plusDays(1));
-//    }
-//
-//    private VisitTreatmentStatus treatmentStatusFor(LocalDate date) {
-//        final LocalDate today = timeSource.now().toLocalDate();
-//        if (date.equals(today)) {
-//            return VisitTreatmentStatus.IN_PROGRESS;
-//        }
-//        else if (date.isAfter(today)) {
-//            return VisitTreatmentStatus.UPCOMING;
-//        }
-//        else {
-//            return VisitTreatmentStatus.DONE;
-//        }
-//    }
-//
-//    private VisitType randomVisitType() {
-//        int pick = random().nextInt(VisitType.values().length);
-//        return VisitType.values()[pick];
-//    }
-//
-//    private String randomDescription() {
-//        return randomOfList(
-//                petclinicTestdataProperties.getDescriptionOptions()
-//        ).trim();
-//    }
-//
-//    private Boolean randomArchive(){
-//        return random().nextBoolean();
-//    }
-//
-//    private <T> T randomOfList(List<T> list) {
-//        if (CollectionUtils.isEmpty(list)) {
-//            return null;
-//        }
-//        return list.get(random().nextInt(list.size()));
-//    }
-//
-//    private Random random() {
-//        return new Random();
-//    }
-//
-//    private <T> List<T> list(Class<T> entityClass) {
-//        return dataManager.load(entityClass).all().list();
-//    }
+
+    private static final Logger log = LoggerFactory.getLogger(OperationTestDataCreation.class);
+
+    @Autowired
+    private Messages messages;
+    protected final TimeSource timeSource;
+    protected final DataManager dataManager;
+
+    public OperationTestDataCreation(TimeSource timeSource, DataManager dataManager) {
+        this.timeSource = timeSource;
+        this.dataManager = dataManager;
+    }
+
+    public void createData() {
+
+        if (accountsExists()) {
+            log.info("Account found in DB. Account Test data generation is skipped...");
+            return;
+        }
+        log.info("No Accounts found in the DB. Accounts Test data will be created...");
+        createAccounts();
+    }
+
+    void createAccounts() {
+        for(int i = 0 ; i < 50; i++){
+//            Account account = dataManager.create(Account.class);
+//            account.setArchive(randomArchive());
+//            account.setCurrency(randomCurrency());
+//            account.setFunds((double) 0);
+//            account.setName(Integer.toString(i+1));
+
+            Operation operation = dataManager.create(Operation.class);
+            //operation.setAccount(account);
+            operation.setDate(new Date());
+            operation.setSum(random().nextLong());
+            operation.setCategory(randomCategory());
+            operation.setType(randomType());
+
+            dataManager.save(operation);
+
+            //dataManager.save(account);
+        }
+    }
+
+    private boolean accountsExists() {
+        return !list(Account.class).isEmpty();
+    }
+
+    private Boolean randomArchive(){
+        return random().nextBoolean();
+    }
+
+    private String randomType(){
+        if(random().nextBoolean()){
+            return messages.getMessage("com.company.informationbanksystem.screen.operation/withdrawal");
+        }else{
+            return messages.getMessage("com.company.informationbanksystem.screen.operation/transfer");
+        }
+    }
+
+    private String randomCategory(){
+        List<String> options = Lists.newArrayList(
+                messages.getMessage("com.company.informationbanksystem.screen.operation/accountOpening"),
+                messages.getMessage("com.company.informationbanksystem.screen.operation/contribution"),
+                messages.getMessage("com.company.informationbanksystem.screen.operation/dragMetal"),
+                messages.getMessage("com.company.informationbanksystem.screen.operation/encasement"),
+                messages.getMessage("com.company.informationbanksystem.screen.operation/loan"),
+                messages.getMessage("com.company.informationbanksystem.screen.operation/foreignExchange"));
+        return options.get(random().nextInt(5));
+    }
+
+    private String randomCurrency(){
+        List<String> options = Lists.newArrayList(
+                "AED", "AFN", "ALL", "AMD", "AUD",
+                "BMD", "BHD", "BIF", "BYR", "CAD",
+                "CHF", "CNY", "CUP", "EUR", "GBP",
+                "HKD", "JPY", "KWP", "KZT", "NOK",
+                "RUB", "THB", "TWD", "UAH", "USD",
+                "XAF", "XOF", "XPF", "ZAR", "ZWL");
+        return options.get(random().nextInt(29));
+    }
+
+    private <T> T randomOfList(List<T> list) {
+        if (CollectionUtils.isEmpty(list)) {
+            return null;
+        }
+        return list.get(random().nextInt(list.size()));
+    }
+
+    private Random random() {
+        return new Random();
+    }
+
+    private <T> List<T> list(Class<T> entityClass) {
+        return dataManager.load(entityClass).all().list();
+    }
 }
--- src/main/java/com/company/informationbanksystem/listener/CreateTestDataOnAppStart.java
+++ src/main/java/com/company/informationbanksystem/listener/CreateTestDataOnAppStart.java
@@ -1,6 +1,7 @@
 package com.company.informationbanksystem.listener;
 
 import com.company.informationbanksystem.app.OperationTestDataCreation;
+import io.jmix.core.security.Authenticated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.stereotype.Component;
@@ -12,8 +13,9 @@ public class CreateTestDataOnAppStart {
     @Autowired
     OperationTestDataCreation operationTestDataCreation;
 
+    @Authenticated
     @EventListener
     public void onApplicationStarted(ApplicationStartedEvent event) {
-        // operationTestDataCreation.createData();
+        operationTestDataCreation.createData();
     }
 }
